861bcbc24775f8b3a935e13290b43d2f819901e7
Ashwin <bhanageashwin28@gmail.com>
2025-08-24 13:18:28 +0530

    Cleared the employee model

---
+++ chai_pydantic/01_Foundations/assigments/employee_models.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from typing import Optional, List
+
+app = FastAPI()
+
+class Employee(BaseModel):
+    id: int
+    name: str = Field(..., min_length=3, description="The name of the employee")
+    department: Optional[str] = "General"
+    salary: float = Field(..., ge=10000)  # salary >= 10000
+
+# In-memory storage
+employees: List[Employee] = []
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+# POST: Register employee
+@app.post("/register")
+def register_employee(employee: Employee):
+    employees.append(employee)
+    return {"message": f"Employee named {employee.name} has been registered"}
+
+# GET: Get all employees
+@app.get("/employees")
+def get_employees():
+    return employees
